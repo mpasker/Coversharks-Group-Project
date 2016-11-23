ff248796bee80f89a900e06308094c6c34da8d04
Matt Pasker <matthewpasker@Matthews-MacBook-Pro.local>
2016-11-23 09:37:11 -0600

    initial project setup

---
+++ js/main.js
@@ -0,0 +1,25 @@
+var app = angular.module('CoverSharks', ['ngRoute', 'CoverSharks.controllers']);
+
+app.config(['$routeProvider', 
+    function($routeProvider){
+        $routeProvider
+            .when('/home',{
+                templateUrl: 'views/home.html',
+                controller: 'homeController'
+            })
+            .when('/about',{
+                templateUrl: 'views/about.html',
+                controller: 'aboutController'
+            })
+            .when('/promotions',{
+                templateUrl: 'views/promotions.html',
+                controller: 'promotionsController'
+            })
+            .when('/howtoplay',{
+                templateUrl: 'views/howtoplay.html',
+                controller: 'howtoplayController'
+            })
+            .otherwise({
+                redirectTo: '/home'
+            })
+    }]);
